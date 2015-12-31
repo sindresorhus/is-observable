483f33aed5248f09b2d119948268bbcf30c0fdde
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:53:31 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
 import test from 'ava';
 import zenObservable from 'zen-observable';
-import fn from './';
+import m from './';
 
 // for `zen-observable` on Node.js 0.10
 global.Promise = Promise;
 
 test(t => {
-	t.true(fn(zenObservable.of(1, 2)));
-	t.false(fn(null));
-	t.false(fn({foo: true}));
-	t.false(fn(function () {}));
-	t.end();
+	t.true(m(zenObservable.of(1, 2)));
+	t.false(m(null));
+	t.false(m({foo: true}));
+	t.false(m(() => {}));
 });
