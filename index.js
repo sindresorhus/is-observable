877aa30099960ad2d891cd2e394f4ab75f7dbfe5
Sindre Sorhus <sindresorhus@gmail.com>
2017-11-03 12:23:19 +0700

    Upgrade `symbol-observable`
    Closes #3
    Fixes #2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
-const symbolObservable = require('symbol-observable');
+const symbolObservable = require('symbol-observable').default;
 
 module.exports = fn => Boolean(fn && fn[symbolObservable]);
