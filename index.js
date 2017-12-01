0797285a59ec2714bc04274fc2419fab9faedf7f
Robin van der Vleuten <robin@webstronauts.co>
2017-12-01 15:51:45 +0100

    Made code compatible with ES5

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
-const symbolObservable = require('symbol-observable').default;
+var symbolObservable = require('symbol-observable').default;
 
-module.exports = fn => Boolean(fn && fn[symbolObservable]);
+module.exports = function (fn) {
+  return Boolean(fn && fn[symbolObservable]);
+}
