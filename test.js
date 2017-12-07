9ad33b8a1d76319c6e5cb73e848cfb2d69df2ed0
Sindre Sorhus <sindresorhus@gmail.com>
2017-12-07 22:29:46 +0100

    Test on more Observable module
    From #6

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import zenObservable from 'zen-observable';
 import Rx from 'rxjs';
 import * as most from 'most';
 import {Stream as xstream} from 'xstream';
+import {IndefiniteObservable} from 'indefinite-observable';
 import m from '.';
 
 test(t => {
@@ -10,6 +11,8 @@ test(t => {
 	t.true(m(Rx.Observable.of(1)));
 	t.true(m(most.of(1)));
 	t.true(m(xstream.of(1)));
+	t.true(m(new IndefiniteObservable(() => {})));
+
 	t.false(m(null));
 	t.false(m({foo: true}));
 	t.false(m(() => {}));
