e15b83548c558d7ca7fecac08b6b599cfd86538b
Sindre Sorhus <sindresorhus@gmail.com>
2017-12-07 20:32:23 +0100

    More tests

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 import test from 'ava';
 import zenObservable from 'zen-observable';
+import Rx from 'rxjs';
+import * as most from 'most';
+import {Stream as xstream} from 'xstream';
 import m from '.';
 
 test(t => {
-	t.true(m(zenObservable.of(1, 2)));
+	t.true(m(zenObservable.of(1)));
+	t.true(m(Rx.Observable.of(1)));
+	t.true(m(most.of(1)));
+	t.true(m(xstream.of(1)));
 	t.false(m(null));
 	t.false(m({foo: true}));
 	t.false(m(() => {}));
